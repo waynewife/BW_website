9e5822f5de8cf75197b73cfd326d74339f4f6bca
Nayan Soni <91832607+nayan2soni@users.noreply.github.com>
2025-04-26 09:00:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Middleware
+
 app.use(express.json());
 
-// Serve static files (including books.json)
 app.use(express.static(path.join(__dirname)));
 
-//upload pdf
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Existing routes (e.g., user routes)
 const userRoutes = require('./routes/user');
 app.use('/api/users', userRoutes);
 
